45bcb18d5b6a788fef27679684bce1ca202d1966
Praveenpadidapu <pvpk1127@gmail.com>
2026-05-17 13:56:40 +0530

    Fix next.config.ts type signature by moving outputFileTracingIncludes to top level

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**/*': ['./dev.db'],
-    },
+  outputFileTracingIncludes: {
+    '/*': ['./dev.db'],
   },
 };
 
 export default nextConfig;
+
 
